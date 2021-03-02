115c1b52b4a30057fa5a2d8ef90e3b41245110c5
NoahExploits <77709966+NoahExploits@users.noreply.github.com>
2021-03-02 16:49:14 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord - requestAnimationFrame('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => )
+    console.log('RandomBot is online')
+});
+
+
+
+client.login('ODE2NDI4OTY0MjkzMTE1OTE0.YD60uQ.XIqgqvg3uS27wiM7Et1XGzCBKzM')
